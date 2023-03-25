06b9b7e33785e949f6a20b653369148cd4213b68
Serhiy Hvala <serhiyhvaladew@gmail.com>
2023-03-25 11:40:05 +0200

    Create About Us Component

--- src/components/AboutUs/index.tsx
+++ src/components/AboutUs/index.tsx
@@ -1,7 +1,28 @@
+import line from '@img/line.png'
+import Button from '@ui/Button'
+
 import styles from './aboutUs.module.scss'
 
 const AboutUs = () => {
-	return <div className={styles.container}>AboutUs</div>
+	return (
+		<div className={styles.container}>
+			<div className={styles.section}>
+				<div className={styles.leftSection}></div>
+				<div className={styles.rightSection}>
+					<h1 className={styles.title}>Think outside the square space</h1>
+					<div className={styles.whatWeDo}>
+						<img className={styles.line} src={line} alt='' />
+						<h2 className={styles.subtitle}>Who we Are</h2>
+					</div>
+					<p className={styles.description}>
+						A creative group of designers and developers with passions for the
+						arts
+					</p>
+					<Button width={120}>See our works</Button>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default AboutUs

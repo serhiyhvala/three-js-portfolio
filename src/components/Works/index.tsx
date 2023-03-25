11945b2e781c12e8c7cbbf9f578d76f41f4a738c
Serhiy Hvala <serhiyhvaladew@gmail.com>
2023-03-25 12:30:49 +0200

    Create Works Component

--- src/components/Works/index.tsx
+++ src/components/Works/index.tsx
@@ -1,7 +1,25 @@
+import { WorksList } from '@constants/index'
+import List from '@ui/List'
+
 import styles from './works.module.scss'
 
 const Works = () => {
-	return <div className={styles.container}>Works</div>
+	return (
+		<div className={styles.container}>
+			<div className={styles.section}>
+				<div className={styles.leftSide}>
+					<List flexDirection='column'>
+						{WorksList.map(item => (
+							<li className={styles.listItem} key={item.id}>
+								{item.title}
+							</li>
+						))}
+					</List>
+				</div>
+				<div className={styles.rightSide}></div>
+			</div>
+		</div>
+	)
 }
 
 export default Works

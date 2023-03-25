bd2b2d8127046d455a3f6646b2cfe02905bf12d7
Serhiy Hvala <serhiyhvaladew@gmail.com>
2023-03-25 14:24:43 +0200

    Create Email Js Form

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -1,33 +1,56 @@
 import Map from '@components/Map'
+import email from '@emailjs/browser'
 import Button from '@ui/Button'
+import { FormEvent, useRef, useState } from 'react'
 
 import styles from './contact.module.scss'
 
 const Contact = () => {
+	const [loading, setLoading] = useState(false)
+	const form = useRef<string | HTMLFormElement>('')
+	const handleSubmitForm = (e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault()
+		setLoading(true)
+		email
+			.sendForm(
+				import.meta.env.VITE_SERVICE_ID,
+				import.meta.env.VITE_TEMPLATE_ID,
+				form.current,
+				import.meta.env.VITE_PUBLIC_KEY
+			)
+			.then(result => console.log(result))
+			.catch(error => console.error(error))
+			.finally(() => {
+				setLoading(false)
+			})
+	}
 	return (
 		<div className={styles.container}>
 			<div className={styles.section}>
 				<div className={styles.leftSide}>
-					<form className={styles.form}>
+					<form ref={form} className={styles.form} onSubmit={handleSubmitForm}>
 						<h1 className={styles.title}>Contact Us</h1>
 						<input
 							type='text'
 							placeholder='Name'
 							required
 							className={styles.input}
+							name='name'
 						/>
 						<input
 							type='email'
 							placeholder='Email'
 							required
 							className={styles.input}
+							name='email'
 						/>
 						<textarea
 							className={styles.textArea}
 							placeholder='Write your message'
 							rows={10}
-						></textarea>
-						<Button width='100%'>Send</Button>
+							name='message'
+						/>
+						<Button width='100%'>{loading ? 'Sending...' : 'Send'}</Button>
 					</form>
 				</div>
 				<div className={styles.rightSide}>

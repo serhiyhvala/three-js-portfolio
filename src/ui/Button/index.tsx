3d9032cd70268ea61b5ce71e463923c2b95df728
Serhiy Hvala <serhiyhvaladew@gmail.com>
2023-03-25 11:39:58 +0200

    Update Button UI Component Props

--- src/ui/Button/index.tsx
+++ src/ui/Button/index.tsx
@@ -2,8 +2,17 @@ import { FC, ReactNode } from 'react'
 
 import styles from './button.module.scss'
 
-const Button: FC<{ children: ReactNode }> = ({ children }) => {
-	return <button className={styles.button}>{children}</button>
+type ButtonProps = {
+	children: ReactNode
+	width?: number
+}
+
+const Button: FC<ButtonProps> = ({ children, width }) => {
+	return (
+		<button className={styles.button} style={{ width: width }}>
+			{children}
+		</button>
+	)
 }
 
 export default Button
